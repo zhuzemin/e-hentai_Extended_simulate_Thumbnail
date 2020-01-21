6dbc89965f310a52f69580d6f5fd833c3a046689
asd <zhuzemin4@hotmail.com>
2020-01-22 03:05:08 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+#e-hentai Extended simulate Thumbnail and Highlight Tag
+
+Compatibility:
+Firefox/Chrome,
+Greasemonkey/Tampermonkey
+
+
+![effect](https://github.com/zhuzemin/e-hentai_Extended_simulate_Thumbnail/raw/master/Screenshot-2020-1-22.jpg)
+
+![setting](https://github.com/zhuzemin/e-hentai_Extended_simulate_Thumbnail/raw/master/Screenshot-2020-1-8.jpg)
---
+++ e-hentai_Extended_simulate_Thumbnail.user.js
@@ -0,0 +1,166 @@
+﻿// ==UserScript==
+// @name        e-hentai Extended simulate Thumbnail and Highlight Tag
+// @namespace   e-hentai_Extended_simulate_Thumbnail
+// @supportURL  https://github.com/zhuzemin
+// @description E-Hentai Extended模仿THumbnail, 并且高亮Tag
+// @include     https://exhentai.org/
+// @include     https://e-hentai.org/
+// @include     https://exhentai.org/?*
+// @include     https://e-hentai.org/?*
+// @include     https://exhentai.org/tag/*
+// @include     https://e-hentai.org/tag/*
+// @version     1.0
+// @grant       GM_xmlhttpRequest
+// @grant         GM_registerMenuCommand
+// @grant         GM_setValue
+// @grant         GM_getValue
+// @run-at      document-start
+// @author      zhuzemin
+// @license     Mozilla Public License 2.0; http://www.mozilla.org/MPL/2.0/
+// @license     CC Attribution-ShareAlike 4.0 International; http://creativecommons.org/licenses/by-sa/4.0/
+// ==/UserScript==
+var config = {
+  'debug': false
+}
+var debug = config.debug ? console.log.bind(console)  : function () {
+};
+
+  // setting User Preferences
+  function setUserPref(varName, defaultVal, menuText, promtText, sep){
+    GM_registerMenuCommand(menuText, function() {
+      var val = prompt(promtText, GM_getValue(varName, defaultVal));
+      if (val === null)  { return; }  // end execution if clicked CANCEL
+      // prepare string of variables separated by the separator
+      if (sep && val){
+        var pat1 = new RegExp('\\s*' + sep + '+\\s*', 'g'); // trim space/s around separator & trim repeated separator
+        var pat2 = new RegExp('(?:^' + sep + '+|' + sep + '+$)', 'g'); // trim starting & trailing separator
+        //val = val.replace(pat1, sep).replace(pat2, '');
+      }
+      //val = val.replace(/\s{2,}/g, ' ').trim();    // remove multiple spaces and trim
+      GM_setValue(varName, val);
+      // Apply changes (immediately if there are no existing highlights, or upon reload to clear the old ones)
+      //if(!document.body.querySelector(".THmo")) THmo_doHighlight(document.body);
+      //else location.reload();
+    });
+  }
+  
+  // prepare UserPrefs
+  setUserPref(
+  'tags',
+  'chinese;',
+  'Set Highlight Tags',
+  `Set Highlight Tags, split with ";". Example: "mmf threesome; chinese"`,	  
+  ','
+  );
+
+
+CreateStyle=function(){
+  debug("Start: CreateStyle");
+  var style=document.createElement("style");
+  style.setAttribute("type","text/css");
+  style.innerHTML=`
+.glowbox {
+     background: #4c4c4c; 
+    //width: 400px;
+    margin: 40px 0 0 40px;
+    padding: 10px;
+    -moz-box-shadow: 0 0 5px 5px #FFFF00;
+    -webkit-box-shadow: 0 0 5px 5px #FFFF00;
+    box-shadow: 0 0 5px 5px #FFFF00;
+}
+`;
+  debug("Processing: CreateStyle");
+  var head=document.querySelector("head");
+  head.insertBefore(style,null);
+  debug("End: CreateStyle");
+}
+function SetExtended(){
+    var hostname=getLocation(window.location.href).hostname;
+    var select=document.querySelector("select");
+    var options=select.querySelectorAll("option");
+    for(var option of options){
+        var value=option.getAttribute("value");
+        var selected=option.getAttribute("selected");
+        if(value=="e"){
+            if(selected=="selected"){
+                break;
+            }
+            else{
+                window.location.href="https://"+hostname+"/?inline_set=dm_"+value;
+            }
+        }
+    }
+
+}
+
+
+function getLocation(href) {
+    var l = document.createElement("a");
+    l.href = href;
+    return l;
+}
+
+var init = function () {
+    SetExtended();
+  var LastDivNum=0;
+    var tags;
+  try{
+    tags=GM_getValue("tags").split(";");
+  }catch(e){
+    debug("Not set tags.");
+  }
+  if(tags==undefined){
+      var tags=[];
+
+  }
+  CreateStyle();
+    var div=document.querySelector("div.ido");
+    div.style="max-width:1370px";
+    setInterval(function(){
+        var tables=document.querySelectorAll("table.itg.glte");
+  if(LastDivNum<tables.length){
+      debug("here");
+      var table=tables[LastDivNum];
+      var tbody=table.querySelector("tbody");
+      tbody.className="itg gld";
+      tbody.style="width:1323px";
+  for (var i = 0; i < tbody.childNodes.length; i++) {
+        var tr=tbody.childNodes[i];
+        tr.className="gl1t";
+        tr.style="min-width:250px !important;width:263px !important;";
+        var detail = tr.querySelector("div.gl3e");
+        detail.className="gl3t";
+        var star=detail.querySelector("div.ir");
+        star.style.margin="auto";
+        var thumb=tr.querySelector("td.gl1e");
+        thumb.firstChild.style="height:340px;";
+        thumb.insertBefore(detail,null);
+        var div=tr.querySelector("div.gl4e.glname");
+        table=div.childNodes[1].querySelector("table");
+        if(table!=null){
+            var TagListCurrent=table.querySelectorAll("div");
+            for(var TagCurrent of TagListCurrent){
+                if(tags.length==0){
+                    break;
+                }
+                for(var tag of tags){
+                    debug("Highlight: "+tag);
+                    if(tag.length>1){
+                        if(TagCurrent.innerText==tag.trim()){
+                            TagCurrent.className +=" glowbox";
+                            break;
+                        }
+                    }
+                }
+
+            }
+
+        }
+  }
+    LastDivNum=tables.length;
+
+  }
+    }, 2000);
+
+}
+window.addEventListener('DOMContentLoaded', init);
